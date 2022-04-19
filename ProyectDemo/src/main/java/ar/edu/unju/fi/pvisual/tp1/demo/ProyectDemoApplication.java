f8a98c54ef1662d3ac0b50797cdecee74ddb069b
facu_ <facu_@LAPTOP-E09C0CEI>
2022-04-19 00:50:53 -0300

    proyectdemo

---
+++ ProyectDemo/src/main/java/ar/edu/unju/fi/pvisual/tp1/demo/ProyectDemoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.tp1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectDemoApplication.class, args);
+	}
+
+}
