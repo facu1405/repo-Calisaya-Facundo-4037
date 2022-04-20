7363136c4ecea6c6e23fb60f4c00c7e74d36bc34
facu_ <facu_@LAPTOP-E09C0CEI>
2022-04-19 21:03:49 -0300

    agregamos clases

--- ProyectDemo/src/main/java/ar/edu/unju/fi/pvisual/tp1/model/Autor.java
+++ ProyectDemo/src/main/java/ar/edu/unju/fi/pvisual/tp1/model/Autor.java
@@ -2,4 +2,40 @@ package ar.edu.unju.fi.pvisual.tp1.model;
 
 public class Autor {
 
+	private String nombreau;
+	private int edadau;
+	private String nacionalidad;
+	
+	public Autor(String nombreau, int edadau, String nacionalidad) {
+		super();
+		this.nombreau = nombreau;
+		this.edadau = edadau;
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getNombreau() {
+		return nombreau;
+	}
+
+	public void setNombreau(String nombreau) {
+		this.nombreau = nombreau;
+	}
+
+	public int getEdadau() {
+		return edadau;
+	}
+
+	public void setEdadau(int edadau) {
+		this.edadau = edadau;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+	
+	
 }
--- ProyectDemo/src/main/java/ar/edu/unju/fi/pvisual/tp1/model/Libro.java
+++ ProyectDemo/src/main/java/ar/edu/unju/fi/pvisual/tp1/model/Libro.java
@@ -1,5 +1,52 @@
 package ar.edu.unju.fi.pvisual.tp1.model;
 
 public class Libro {
- //
+
+	private String nombreLibro;
+	private String editorial;
+	private int añoEdcion;
+	private Autor autor;
+	
+	public Libro(String nombreLibro, String editorial, int añoEdcion, Autor autor) {
+		super();
+		this.nombreLibro = nombreLibro;
+		this.editorial = editorial;
+		this.añoEdcion = añoEdcion;
+		this.autor = autor;
+	}
+
+	public String getNombreLibro() {
+		return nombreLibro;
+	}
+
+	public void setNombreLibro(String nombreLibro) {
+		this.nombreLibro = nombreLibro;
+	}
+
+	public String getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+
+	public int getAñoEdcion() {
+		return añoEdcion;
+	}
+
+	public void setAñoEdcion(int añoEdcion) {
+		this.añoEdcion = añoEdcion;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+	
+	
+	
 }
